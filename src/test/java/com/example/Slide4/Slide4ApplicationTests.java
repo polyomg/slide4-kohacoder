3cf66b871f8d57fd602923560f646ffd13609079
kohacoder <172766095+kohacoder@users.noreply.github.com>
2025-10-02 14:01:39 +0700

    TS00671-HoangNguyenDangKhoa-Slide4-FirstCommit

---
+++ src/test/java/com/example/Slide4/Slide4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Slide4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Slide4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
